c2a193f024cd743d0a29208071f45bc6b2cb6c9e
uwx <maxine@cybergal.com>
2026-09-20 14:05:35 +0200

    detach from FNA

--- src/FixedMathSharp/Numerics/Vector3d.cs
+++ src/FixedMathSharp/Numerics/Vector3d.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 using MessagePack;
-using Microsoft.Xna.Framework;
 
 namespace FixedMathSharp;
 
